6baf002c2b30be08ac62761a4db142f2cb811598
Peter Lehto <peter@vaadin.com>
2017-02-21 12:02:18 +0200

    Deploy module and proper usage of widgetset and theme

--- root/dashboard-ui/src/main/java/com/vaadin/peter/dashboard/ui/DashboardUI.java
+++ root/dashboard-ui/src/main/java/com/vaadin/peter/dashboard/ui/DashboardUI.java
@@ -1,10 +1,14 @@
 package com.vaadin.peter.dashboard.ui;
 
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.Widgetset;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.UI;
 
 @SpringUI
+@Widgetset("com.vaadin.peter.dashboard.widgetset.DashboardWidgetset")
+@Theme("dashboard")
 public class DashboardUI extends UI {
 
 	@Override
