957d1c3b718b8059179882abedd06cf6c0fda94e
Peter Lehto <peter@vaadin.com>
2017-02-21 11:48:34 +0200

    Initial project maven structure

---
+++ root/dashboard-ui/src/main/java/com/vaadin/peter/dashboard/ui/DashboardUI.java
@@ -0,0 +1,14 @@
+package com.vaadin.peter.dashboard.ui;
+
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.UI;
+
+@SpringUI
+public class DashboardUI extends UI {
+
+	@Override
+	protected void init(VaadinRequest request) {
+
+	}
+}
